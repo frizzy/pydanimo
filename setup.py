79ad190432c2d455d5b92467c2bed0f68c37a881
Bernard van Niekerk <bernard.van.niekerk@icloud.com>
2022-01-21 17:37:05 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydanimo",
+    version="1.0.0-beta.1",
+    author="Bernard van Niekerk",
+    description="Python DynamoDB Abstraction layer",
+    long_description=long_description,
+    url="https://github.com/frizzy/pydanimo",
+    packages=["pydanimo"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "fastapi>=0.72.0",
+        "pydantic>=1.9.0"
+    ],
+    python_requires=">=3.9",
+)
