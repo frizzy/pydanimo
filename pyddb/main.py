ea6fda7b1fac4bb6034bf7471010a7f26e79ba1a
Bernard van Niekerk <bernard.van.niekerk@icloud.com>
2022-02-24 15:42:28 +0100

    Indexes

--- pyddb/main.py
+++ pyddb/main.py
@@ -1,4 +1,3 @@
-from io import UnsupportedOperation
 from typing import Optional
 from pydantic import BaseModel, root_validator, create_model
 from pyddb.attributes import KeyAttribute, Deserializable, Serializable
@@ -15,6 +14,10 @@ def to_str(self):
 
 class BaseItem(BaseModel):
 
+    class Settings:
+        keys = []
+        indexes = {}
+
     @classmethod
     def key(cls, item=None, **kwargs):
         args = item.dict(exclude_unset=True) if item else kwargs
@@ -27,13 +30,25 @@ class BaseItem(BaseModel):
         return ItemKeyClass(**args)
 
     @classmethod
+    def index_key(cls, name: str, item=None, **kwargs):
+        args = item.dict(exclude_unset=True) if item else kwargs
+        ItemKeyClass = type(
+            'ItemKey',
+            (cls, ),
+            {'__str__': to_str} if len(args) == 1 else {},
+            item_keys=args.keys(),
+            index=name
+        )
+        return ItemKeyClass(**args)
+
+    @classmethod
     def match(cls, name, **kwargs):
         if issubclass(cls.__fields__[name].type_, BaseModel):
             _type = cls.__fields__[name].type_
         elif cls.__fields__[name].sub_fields and issubclass(cls.__fields__[name].sub_fields[0].type_, BaseModel):
             _type = cls.__fields__[name].sub_fields[0].type_
         else:
-            raise UnsupportedOperation(f'{name} is not a matchable custom attribute')
+            raise ValueError(f'{name} is not a matchable custom attribute')
 
         MatchClass = create_model(f'{_type.__name__}Match', __base__=_type)
         for field in MatchClass.__fields__.values():
@@ -50,9 +65,18 @@ class BaseItem(BaseModel):
         return as_dict(model, **kwargs)
 
     def __init_subclass__(cls, **kwargs) -> None:
+        index = kwargs.pop('index', None)
         if (item_keys := kwargs.pop('item_keys', None)):
+            if index and index not in cls.Settings.indexes:
+                raise ValueError(f'{index} is not a valid index')
             for key in list(cls.__fields__.keys()):
-                if not issubclass(cls.__fields__[key].type_, KeyAttribute) or key not in item_keys:
+                if key not in item_keys:
+                    cls.__fields__.pop(key)
+                    continue
+                if index and key not in cls.Settings.indexes[index]:
+                    cls.__fields__.pop(key)
+                    continue
+                if not index and not issubclass(cls.__fields__[key].type_, KeyAttribute):
                     cls.__fields__.pop(key)
                     continue
                 cls.__fields__[key].outer_type_ = Optional
--- tests/test_models.py
+++ tests/test_models.py
@@ -25,9 +25,32 @@ def test_model_union():
         pk: KeyAttribute[str]
         sk: KeyAttribute[SortKey]
 
+    ItemUnion = Union[ConcreteItemA, ConcreteItemB]
+
     class Item(BaseModel):
-        __root__: Union[ConcreteItemA, ConcreteItemB]
+        item: ItemUnion
 
-    item = Item.parse_obj({"pk": "hello", "sk": f"type_b#{uuid4()}"}).__root__
+    item = Item.parse_obj({"item": {"pk": "hello", "sk": f"type_b#{uuid4()}"}})
 
-    assert type(item) is ConcreteItemB
+    assert type(item.item) is ConcreteItemB
+
+
+def test_indexes():
+
+    class Foo(BaseItem):
+
+        class Settings:
+            keys = ['id']
+            indexes = {
+                'gsi_1': ['gsi_1_pk', 'gsi_1_sk']
+            }
+
+        id: KeyAttribute[str]
+        something: int
+        gsi_1_pk: str
+        gsi_1_sk: str
+
+    foo = Foo(id='hello', something=2, gsi_1_pk='moo', gsi_1_sk='cow')
+
+    assert Foo.index_key('gsi_1', gsi_1_pk='cheese') == {'gsi_1_pk': 'cheese'}
+    assert str(Foo.index_key('gsi_1', gsi_1_sk='cheese')) == 'cheese'
