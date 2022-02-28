ae294e56e4e7300a633574e5894e044dfe54f454
Bernard van Niekerk <bernard.van.niekerk@icloud.com>
2022-02-28 12:40:10 +0100

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyddb",
-    version="0.1.14",
+    version="1.0.0",
     author="Bernard van Niekerk",
     description="Python DynamoDB Abstraction layer",
     long_description=long_description,
